37fc6718f4e64827af9cf49045603ac813bb4ab1
Nicole MacLean <nicole.mac0404@gmail.com>
2022-08-04 18:41:43 -0700

    All routes working with simple get request

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,1 +1,33 @@
 // Get user where email and password match
+
+const express = require("express");
+const router = express.Router();
+
+module.exports = (db) => {
+  // Retrieve information
+  router.get("/", (req, res) => {
+    db.Users.get();
+    res.send("Square Life ◼️ users...");
+  });
+  // Update information
+  router.put("/", (req, res) => {
+    res.send("Square Life ◼️ users...");
+  });
+  // Creates new information
+  router.post("/", (req, res) => {
+    res.send("Square Life ◼️ users...");
+  });
+  // Delete information
+  router.delete("/", (req, res) => {
+    res.send("Square Life ◼️ users...");
+  });
+
+  db.Users.create({
+    email: "nicole.mac0404@gmail.com",
+    username: "Nicole",
+    password: "123",
+  }).then(() => {
+    console.log("saved");
+  });
+  return router;
+};
--- server/server.js
+++ server/server.js
@@ -10,50 +10,54 @@ const config = require("./config/configuration");
 
 const db = require("./models/index");
 
-db.Users.create({
-  email: "nicole.mac0404@gmail.com",
-  username: "Nicole",
-  password: "123",
-}).then(() => {
-  console.log("saved");
-});
+// db.Users.create({
+//   email: "nicole.mac0404@gmail.com",
+//   username: "Nicole",
+//   password: "123",
+// }).then(() => {
+//   console.log("saved");
+// });
 
-// const app = express();
-// app.use(bodyParser.urlencoded({ extended: false }));
-// app.set("view engine", "ejs");
-// app.use(helmet());
-// app.use(
-//   cookieSession({
-//     name: "session",
-//     keys: ["password", "cookie", "squarelife", "anotherkey"],
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.set("view engine", "ejs");
+app.use(helmet());
+app.use(
+  cookieSession({
+    name: "session",
+    keys: ["password", "cookie", "squarelife", "anotherkey"],
 
-//     // Cookie Opti
-//     maxAge: 24 * 60 * 60 * 1000, // 24 hours
-//   })
-// );
-// app.use(morgan("dev"));
+    // Cookie Opti
+    maxAge: 24 * 60 * 60 * 1000, // 24 hours
+  })
+);
+app.use(morgan("dev"));
 
 // // Password encryption
 // const salt = bcrypt.genSaltSync(10);
 
 // // Routes
-// // const users = require("./routes/users");
-// // const tasks = require("./routes/tasks");
-// // const squares = require("./routes/squares");
+const users = require("./routes/users");
+const tasks = require("./routes/tasks");
+const squares = require("./routes/squares");
 // // const loginRoute = require("./routes/login");
 // // const logoutRoute = require("./routes/logout");
 
 // // Mount all resource routes
-// // app.use("/task", tasks(db));
-// // app.use("/user", users(db));
-// // app.use("/squares", squares(db));
+app.use("/tasks", tasks(db));
+app.use("/users", users(db));
+app.use("/squares", squares(db));
 // // app.use("/login", loginRoute(db));
 // // app.use("/logout", logoutRoute(db));
 
-// app.get("/", function (req, res) {
-//   res.send("Square Life ◼️");
+app.get("/", (req, res) => {
+  res.send("Square Life ◼️");
+});
+
+// app.get("/user", (req, res) => {
+//   res.send("Square Life ◼️ users...");
 // });
 
-// app.listen(config.port, () => {
-//   console.log(`Example app listening on port ${config.port}`);
-// });
+app.listen(config.port, () => {
+  console.log(`Example app listening on port ${config.port}`);
+});
